9012a987afb3d4e970b58b448097384789594290
James Kamz <jacquesamouzou9@gmail.com>
2023-08-17 16:11:38 +0000

    adding localhost

--- Oliveshop/settings.py
+++ Oliveshop/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY =env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =env('DEBUG')
 
-ALLOWED_HOSTS =['oliveshop.onrender.com']
+ALLOWED_HOSTS =['oliveshop.onrender.com', '127.0.0.1']
 
 
 # Application definition

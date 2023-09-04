988d8ecbeecc6ae8d35794ecdafc21eeea5053bd
James kamz <81109024+JamesKamz@users.noreply.github.com>
2023-09-04 18:41:31 +0000

    Update settings.py

--- Oliveshop/settings.py
+++ Oliveshop/settings.py
@@ -5,7 +5,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env=environ.Env(
-    DEBUG=(bool, True)
+    DEBUG=(bool, False)
 )
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
